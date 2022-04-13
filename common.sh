9c041e1420afcd9f87f81a3e09d9df2d161228a5
Sebastian Bolanos <sebasheston@protonmail.com>
2022-04-13 07:03:21 -0600

    Add common values for one node

---
+++ common.sh
@@ -0,0 +1,2 @@
+# Obtained by running go run in https://github.com/sbc64/multiaddr-node-id-calculator
+one=('bbf358ab08ab29d70b6b20845e4aa417124bb8051ecdbaf4f822bba18f28f7fb' '16Uiu2HAkxthSUou6qdD5igzt6E4niDRbsXGTU6GpKNGKRahv6TTX' '6001' '8546')
